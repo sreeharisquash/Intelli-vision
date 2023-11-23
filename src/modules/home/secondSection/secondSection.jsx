c7ab183735c1e44c276d8e0ccc2e732e19af2a44
pravin <pravin@squashapps.com>
2023-11-23 09:50:56 +0530

    error-fix

--- src/modules/home/secondSection/secondSection.jsx
+++ src/modules/home/secondSection/secondSection.jsx
@@ -118,18 +118,18 @@ export default function Section2() {
     },
   ];
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   arrows: false,
     //   centerMode: true,
     //   centerPadding: "15px"
-  };
+  // };
 
   return (
     <>
